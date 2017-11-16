adaf605bba986b4c4488c280a023799fe1c2e060
Maartenleon <maarten.stienstra@gmail.com>
2017-11-16 16:09:37 +0100

    added constructor and addBooking to BookingController
    added constructor and addBooking to BookingController

--- src/main/java/com/SEtrack/Hotel/controllers/BookingController.java
+++ src/main/java/com/SEtrack/Hotel/controllers/BookingController.java
@@ -1,4 +1,31 @@
 package com.SEtrack.Hotel.controllers;
 
+import com.SEtrack.Hotel.models.Booking;
+import java.util.ArrayList;
+
+/**
+ * represents the controller for bookings
+ * @author mstienst
+ */
 public class BookingController {
+
+    private ArrayList bookingList;
+
+    /**
+     * constructor of booking controller
+     */
+    public BookingController (){
+        this.bookingList = new ArrayList<Booking>();
+    }
+
+    /**
+     * method to add booking to bookingList
+     * @param booking Booking: the booking object
+     */
+    public void addBooking (Booking booking){
+
+        bookingList.add(booking);
+        System.out.println("Boeking is toegevoegd. Boekingnummer: " + booking.getBookingNr());
+    }
+
 }
