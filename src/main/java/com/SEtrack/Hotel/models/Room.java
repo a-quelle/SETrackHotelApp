18a6e1ccee791a2cffb915b3411d290d2c0081df
capgeminiKoen <33454421+capgeminiKoen@users.noreply.github.com>
2017-11-16 15:31:39 +0100

    Added Room functionality including getter and setter

--- src/main/java/com/SEtrack/Hotel/models/Room.java
+++ src/main/java/com/SEtrack/Hotel/models/Room.java
@@ -1,4 +1,62 @@
 package com.SEtrack.Hotel.models;
 
+import com.SEtrack.Hotel.controllers.RoomSize;
+import com.SEtrack.Hotel.controllers.RoomType;
+
+import java.time.LocalDate;
+
 public class Room {
+
+    private int bookingNumber;
+    private RoomType roomType;
+    private RoomSize roomSize;
+    private boolean available;
+    private LocalDate dateAvailable;
+
+    public Room(int bookingNumber, RoomType roomType, RoomSize roomSize, LocalDate dateAvailable){
+        this.dateAvailable = dateAvailable;
+        this.roomSize = roomSize;
+        this.roomType = roomType;
+        this.dateAvailable = dateAvailable;
+    }
+
+    public int getBookingNumber() {
+        return bookingNumber;
+    }
+
+    public void setBookingNumber(int bookingNumber) {
+        this.bookingNumber = bookingNumber;
+    }
+
+    public RoomType getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(RoomType roomType) {
+        this.roomType = roomType;
+    }
+
+    public RoomSize getRoomSize() {
+        return roomSize;
+    }
+
+    public void setRoomSize(RoomSize roomSize) {
+        this.roomSize = roomSize;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public LocalDate getDateAvailable() {
+        return dateAvailable;
+    }
+
+    public void setDateAvailable(LocalDate dateAvailable) {
+        this.dateAvailable = dateAvailable;
+    }
 }
