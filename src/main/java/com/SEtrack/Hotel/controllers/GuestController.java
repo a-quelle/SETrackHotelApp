45f39a11793a007c20f6b8cb4feaab07ea886850
Anton Quelle <antonquelle@gmail.com>
2017-11-16 15:36:53 +0100

    Getters and setters and variables in Guest

--- src/main/java/com/SEtrack/Hotel/controllers/GuestController.java
+++ src/main/java/com/SEtrack/Hotel/controllers/GuestController.java
@@ -1,4 +1,12 @@
 package com.SEtrack.Hotel.controllers;
 
+import java.util.ArrayList;
+import com.SEtrack.Hotel.models.Guest;
+
 public class GuestController {
+
+    ArrayList<Guest> guestList= new ArrayList<>();
+
+
+
 }
