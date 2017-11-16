afbe97ba9423fafd475168354ec3a957e777a52f
Daniel <33453748+DCO-SE-Track@users.noreply.github.com>
2017-11-16 15:39:29 +0100

    Basic search function for guests.

--- src/main/java/com/SEtrack/Hotel/controllers/GuestController.java
+++ src/main/java/com/SEtrack/Hotel/controllers/GuestController.java
@@ -1,4 +1,28 @@
 package com.SEtrack.Hotel.controllers;
 
+import com.SEtrack.Hotel.models.Guest;
+
+import java.util.ArrayList;
+
 public class GuestController {
+
+    private ArrayList<Guest> guestList;
+
+    public GuestController(){
+        guestList = new ArrayList<>();
+    }
+
+    public ArrayList<Guest> getGuestList(){
+        return getGuestList();
+    }
+
+    public ArrayList<Guest> getGuestByName(String s){
+        ArrayList<Guest> searchResults = new ArrayList<>();
+        for(int i = 0; i < guestList.size(); i++) {
+            if(guestList.get(i).getFullName().contains(s)){
+                searchResults.add(guestList.get(i));
+            }
+        }
+        return searchResults;
+    }
 }
--- src/main/java/com/SEtrack/Hotel/models/Guest.java
+++ src/main/java/com/SEtrack/Hotel/models/Guest.java
@@ -1,4 +1,8 @@
 package com.SEtrack.Hotel.models;
 
 public class Guest {
+
+    public String getFullName(){
+        return firstName + " " + lastName;
+    }
 }
