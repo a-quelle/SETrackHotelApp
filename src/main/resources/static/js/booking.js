6f02d02feb78f9cb7361d816c2264764ff86599e
Anton Quelle <antonquelle@gmail.com>
2017-11-22 15:32:03 +0100

    Finished validating Form

--- src/main/resources/static/js/booking.js
+++ src/main/resources/static/js/booking.js
@@ -1,3 +1,27 @@
+//Function to get the list of guests from the server. This functionality is not present serverside yet.
+
+/*
+function getGuests() {
+    $("#data").empty();
+    console.log("getting data...")
+
+    $.ajax({
+        url:"http://localhost:8080/api/books/all",
+        type:"get",
+        success: function(books) {
+            console.log("This is the data: " + books);
+
+            $.each(books,function(index, book){
+                console.log(book.id + " " + book.title + " " + book.author);
+                $("#data").append("Id: " +book.id + ", title: " +book.title + ", author: " +book.author + "<br>");
+            })
+        }
+    })
+
+}
+*/
+//hardcoded list of guests absent the previous functionality.
+
 var guests = [
     {
         "guestNr":1320123,
@@ -11,53 +35,115 @@ var guests = [
     }
 ]
 
-//Fill the guest select field with all the elements in the var guests defined above.
+//Fill the guest select field of createBooking.html with all the elements in the var guests defined above.
 
 $(document).ready(function(){
     for(i=0;i<guests.length;i++) {
-        $("#guestSelect").append('<option value="guests[i]">'+guests[i].firstName+' '+guests[i].lastName+'</option>');
+        $("#guestSelect").append('<option value='+i+'>'+guests[i].firstName+' '+guests[i].lastName+'</option>');
     }
 })
+
+//Function to get the list of rooms from the server. This functionality is not present serverside yet.
+
+/*
+function getGuests() {
+    $("#data").empty();
+    console.log("getting data...")
+
+    $.ajax({
+        url:"http://localhost:8080/api/books/all",
+        type:"get",
+        success: function(books) {
+            console.log("This is the data: " + books);
+
+            $.each(books,function(index, book){
+                console.log(book.id + " " + book.title + " " + book.author);
+                $("#data").append("Id: " +book.id + ", title: " +book.title + ", author: " +book.author + "<br>");
+            })
+        }
+    })
+
+}
+*/
+//Hardcoded list of rooms, absent the previous functionality
+var rooms = [
+    {
+        "roomNr":1
+    },
+    {
+        "roomNr":2
+    },
+    {
+        "roomNr":3
+    }
+]
+
+//Fill the rooms select field of createBooking.html with all the elements in the var guests defined above.
+
+$(document).ready(function(){
+    for(i=0;i<rooms.length;i++) {
+        $("#roomSelect").append('<option value='+i+'>'+rooms[i].roomNr+'</option>');
+    }
+})
+
+//Variables that have to be read from the input form
+
+var bookingNr;
+var guest;
+var room;
+var startDate;
+var nrOfNights;
+var checkIn;
 
 //Function that checks all the submitted fields upon clicking submit.
 
 function submitClick () {
-    var bookingNr;
-    var guest;
-    var room;
-    var startDate;
-    var nrOfNights;
-    var checkIn;
-    bookingNr=Number(document.getElementsByName("bookingNr")[0].value);
+    readInput();
+    if(checkInput()) {
+        submitInput();
+    }
+}
+
+//Typechecks all the input fields to make sure they are of the correct type.
+
+function readInput () {
+    bookingNr=$("#bookingNr").val();
+    console.log(bookingNr);
+    nrOfNights=$("#nrOfNights").val();
+    console.log(nrOfNights);
+    guest= guests[$("#guestSelect").val()];
+    console.log(guest);
+    room= rooms[$("#roomSelect").val()];
+    console.log(room);
+    startDate=$("#startDate").val();
+    console.log(startDate);
+    checkIn=$("#checkedIn").val();
+    console.log(checkIn);
+
+}
+
+function checkInput () {
+    var check =true;
+
     if(isNaN(bookingNr)) {
         document.getElementById("bookingNrText").innerHTML="Booking nr: <font color='red'>This has to be a number!</font>";
+        check=false;
     }
     else {
         document.getElementById("bookingNrText").innerHTML="Booking nr:</font>";
-        alert(bookingNr);
     }
-
-    room=Number(document.getElementsByName("roomNr")[0].value);
-    if(isNaN(room)) {
-        document.getElementById("roomNrText").innerHTML="Room nr: <font color='red'>This has to be a number!</font>";
-    }
-    else {
-        document.getElementById("roomNrText").innerHTML="Room nr:</font>";
-        alert(room);
-    }
-    nrOfNights=Number(document.getElementsByName("nrOfNights")[0].value);
     if(isNaN(nrOfNights)) {
-            document.getElementById("nrOfNightsText").innerHTML="Number of nights: <font color='red'>This has to be a number!</font>";
+        document.getElementById("nrOfNightsText").innerHTML="Number of nights: <font color='red'>This has to be a number!</font>";
+        check=false;
     }
     else {
-            document.getElementById("nrOfNightsText").innerHTML="Number of nights:</font>";
-            alert(nrOfNights);
+        document.getElementById("nrOfNightsText").innerHTML="Number of nights:</font>";
     }
 
-    var booking = {
-        "bookingNr":bookingNr,
+    console.log(check);
+    return check;
+}
 
-    }
-
-    return false;
+function submitInput () {
+    console.log("Made it!");
 }
