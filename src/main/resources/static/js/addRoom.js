47748bfe5a9ca9bfa9027e40ef4dd45ec4568243
Maartenleon <maarten.stienstra@gmail.com>
2017-11-30 14:56:25 +0100

    added form validation
    added form validation

--- src/main/resources/static/js/addRoom.js
+++ src/main/resources/static/js/addRoom.js
@@ -1,12 +1,19 @@
 $(document).ready(function(){
 
+    $('#pauperForm').validator().on('submit', function (e) {
+      if (e.isDefaultPrevented()) {
+        // INVALID FORM, DO NOTHING
+        alert("NEE!");
+      } else {
+        // Do something.
+        postForm();
+      }
+    });
+
     // Click function
-    $("#submitButton").click(function(e){
+    function postForm(){
         $("#roomAddedMessage").hide();
         $("#roomNotAddedError").hide();
-
-    // Prevent form submit
-        e.preventDefault();
 
         var roomNumber = $("#roomNumber").val();
         var roomType = $("#roomType").val();
@@ -18,19 +25,6 @@ $(document).ready(function(){
             roomType: roomType,
             roomSize: roomSize
         };
-
-        var valid = true;
-
-        // Check the room number.
-        if(roomNumber == null || roomNumber === "" || roomNumber < 1){
-            $("#roomNumberError").html("Roomnumber must be a number above 0");
-            valid = false;
-        }
-        else{
-            $("#roomNumberError").html("");
-        }
-
-        if(!valid) return;
 
         var roomString = JSON.stringify(room);
 
